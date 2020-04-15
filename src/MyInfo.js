be297a1af441c037d11179b0d0f1ce6e155be72a
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2020-04-15 12:54:17 -0500

    separate files

---
+++ src/MyInfo.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+function() MyInfo {
+  return (
+    <div>
+    <h1>Jess White</h1>
+    <p>This is a blurb about me</p>
+    <ul>
+    <li>Cancun</li>
+    <li>Romania</li>
+    <li>Surinam</li>
+    </ul>
+    </div>
+    )
+}
+
+export default MyInfo
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MyInfo from './MyInfo';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
